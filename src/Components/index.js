141a5ec841a7471fa2aab93172bf22b11b96540b
Endi Oliveira <100172961+endioliveira@users.noreply.github.com>
2023-03-05 17:33:10 -0300

    criando as pastas de componentes, estilos, rotas, constantes, assets e paginas

---
+++ src/Components/index.js
@@ -0,0 +1,4 @@
+export * from "./Header";
+export * from "./CreatePost";
+export * from "./Posts";
+export * from "./Comments";
---
+++ src/Pages/index.js
@@ -0,0 +1,3 @@
+export * from "./feed";
+export * from "./login";
+export * from "./signup";
